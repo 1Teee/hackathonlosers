c5031d3d8b89d6fd555c138f1258da4d8d564b49
Tawab <angrybirdsrock2007@gmail.com>
2025-03-29 10:46:23 -0400

    html almost works

--- app/__init__.py
+++ app/__init__.py
@@ -10,13 +10,31 @@ key = "AIzaSyAlhdlfM5OklUfOHAK8gdgksuYbOwH3d7o"
 def home():
     print("we're home!")
 
-    il = ""
+    il = "" # jay lukose can u add the image link and make it randomly generate please
+    #JLUKOSE ADD CITY INFORMATION
+    # city = CITYNAME
+    # location
+    # all that stuff
+
 
     if request.method == "POST":
-        city = request.form["input"]
-        return render_template('index.html', image_link = il)
+        guess = request.form["input"]
+        r = "CORRECT" #(city == guess)
+        print("result method")
+        return render_template('result.html', c = guess, res = r)
 
     return render_template('index.html', image_link = il)
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    print("running result page!")
+
+    if request.method == "GET":
+        "going back home..."
+        return redirect(url_for("home"))
+    
+    return render_template('result.html')
+
 
 if __name__ == "__main__":
     app.debug = True
