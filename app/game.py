522e84272a494d39b39c3ae9f1eac813be712487
Jonathan Metzler <jmetzler50@stuy.edu>
2025-03-29 10:06:21 -0400

    copied math over

---
+++ app/game.py
@@ -0,0 +1,17 @@
+import math
+
+RADIUS = 6371.0
+
+def toRadians(degree):
+    return degree * (math.pi / 180.0)
+
+def haversine(lat1, lon1, lat2, lon2):
+    lat1 = toRadians(lat1)
+    lon1 = toRadians(lon1)
+    lat2 = toRadians(lat2)
+    lon2 = toRadians(lon2)
+    dLat = lat2 - lat1
+    dLon = lon2 - lon1
+    a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.cos(lat1) * math.cos(lat2) * math.sin(dLon / 2) * math.sin(dLon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return RADIUS * c
